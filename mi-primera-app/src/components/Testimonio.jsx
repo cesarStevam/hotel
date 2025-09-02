4a00771bbc9a14310cd773fb172073353f2932d8
cesar <cesar2005.com@gmail.com>
2025-09-02 15:13:03 -0500

    inicio de la aplicacion

---
+++ mi-primera-app/src/components/Testimonio.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Testimonio (){
+    return (
+        <div className = "contenerdor-testimonio">
+            <img 
+            className = "imagen-testimonio"
+            src = {require("../images/imagen-1.jpg")}
+            alt = "una imagende un pirobo todo feo" />
+            <div className="contenedor-texto-testimonio">
+                <p className="nombre-testimonio">juan carlos roloooo</p>
+                <p className="cargo-testimonio">chatarrero profecional</p>
+                <p className="texto-testimonio">esta mierda me dejo en la inmunda que asko de CurzO</p>
+            </div>
+
+        </div>
+    )
+} 
